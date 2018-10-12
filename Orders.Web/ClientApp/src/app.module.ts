0f60bddfa5b3af93efea63e8577aecf7f2edd7d4
Maksym Kolomiets <p.s.klom@gmail.com>
2018-10-12 11:27:07 +0300

    initial

---
+++ Orders.Web/ClientApp/src/app.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { DxButtonModule } from "devextreme-angular";
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports: [BrowserModule, FormsModule, DxButtonModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Orders.Web/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Orders.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // добавляем сборку через webpack
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                                                {
+                                                    HotModuleReplacement = true
+                                                });
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
